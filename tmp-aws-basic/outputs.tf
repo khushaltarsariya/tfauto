64b0fcd3cee11cbcb81a1758e7e2a822a224a012
khushal <khushaltarsariya123@gmail.com>
2026-03-12 16:17:12 +0530

    deleted

--- tmp-aws-basic/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.aws-basic-instance.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.aws-basic-instance.public_ip
-}
